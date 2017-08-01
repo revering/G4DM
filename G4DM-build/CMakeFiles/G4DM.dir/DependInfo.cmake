a1a88505709105e53e0a742bf0cce84b948c1fbb
Michael Revering <rever025@umn.edu>
2017-08-01 12:00:06 -0500

    Initial commit of basic geant simulation of dark photon interaction.

---
+++ G4DM-build/CMakeFiles/G4DM.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/local/cms/user/revering/geant/G4DM/G4DM.cc" "/local/cms/user/revering/geant/G4DM/G4DM-build/CMakeFiles/G4DM.dir/G4DM.cc.o"
+  "/local/cms/user/revering/geant/G4DM/src/G4DMActionInitialization.cc" "/local/cms/user/revering/geant/G4DM/G4DM-build/CMakeFiles/G4DM.dir/src/G4DMActionInitialization.cc.o"
+  "/local/cms/user/revering/geant/G4DM/src/G4DMDetectorConstruction.cc" "/local/cms/user/revering/geant/G4DM/G4DM-build/CMakeFiles/G4DM.dir/src/G4DMDetectorConstruction.cc.o"
+  "/local/cms/user/revering/geant/G4DM/src/G4DMEventAction.cc" "/local/cms/user/revering/geant/G4DM/G4DM-build/CMakeFiles/G4DM.dir/src/G4DMEventAction.cc.o"
+  "/local/cms/user/revering/geant/G4DM/src/G4DMPhysicsList.cc" "/local/cms/user/revering/geant/G4DM/G4DM-build/CMakeFiles/G4DM.dir/src/G4DMPhysicsList.cc.o"
+  "/local/cms/user/revering/geant/G4DM/src/G4DMPrimaryGeneratorAction.cc" "/local/cms/user/revering/geant/G4DM/G4DM-build/CMakeFiles/G4DM.dir/src/G4DMPrimaryGeneratorAction.cc.o"
+  "/local/cms/user/revering/geant/G4DM/src/G4DMRunAction.cc" "/local/cms/user/revering/geant/G4DM/G4DM-build/CMakeFiles/G4DM.dir/src/G4DMRunAction.cc.o"
+  "/local/cms/user/revering/geant/G4DM/src/G4DMSteppingAction.cc" "/local/cms/user/revering/geant/G4DM/G4DM-build/CMakeFiles/G4DM.dir/src/G4DMSteppingAction.cc.o"
+  "/local/cms/user/revering/geant/G4DM/src/G4DarkPhoton.cc" "/local/cms/user/revering/geant/G4DM/G4DM-build/CMakeFiles/G4DM.dir/src/G4DarkPhoton.cc.o"
+  "/local/cms/user/revering/geant/G4DM/src/G4eDarkBremsstrahlung.cc" "/local/cms/user/revering/geant/G4DM/G4DM-build/CMakeFiles/G4DM.dir/src/G4eDarkBremsstrahlung.cc.o"
+  "/local/cms/user/revering/geant/G4DM/src/G4eDarkBremsstrahlungModel.cc" "/local/cms/user/revering/geant/G4DM/G4DM-build/CMakeFiles/G4DM.dir/src/G4eDarkBremsstrahlungModel.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/local/cms/other/geant4/geant4.10.02.p02/include/Geant4"
+  "/cvmfs/cms.cern.ch/slc6_amd64_gcc493/external/xerces-c/3.1.3/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ G4DM-build/CMakeFiles/G4DM.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/G4DM.dir/G4DM.cc.o"
+  "CMakeFiles/G4DM.dir/src/G4eDarkBremsstrahlungModel.cc.o"
+  "CMakeFiles/G4DM.dir/src/G4DMSteppingAction.cc.o"
+  "CMakeFiles/G4DM.dir/src/G4DarkPhoton.cc.o"
+  "CMakeFiles/G4DM.dir/src/G4DMRunAction.cc.o"
+  "CMakeFiles/G4DM.dir/src/G4DMPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/G4DM.dir/src/G4eDarkBremsstrahlung.cc.o"
+  "CMakeFiles/G4DM.dir/src/G4DMDetectorConstruction.cc.o"
+  "CMakeFiles/G4DM.dir/src/G4DMActionInitialization.cc.o"
+  "CMakeFiles/G4DM.dir/src/G4DMPhysicsList.cc.o"
+  "CMakeFiles/G4DM.dir/src/G4DMEventAction.cc.o"
+  "G4DM.pdb"
+  "G4DM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/G4DM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
